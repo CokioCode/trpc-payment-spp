0b01b1e30aa47d4a1df2459a1a9928051199551c
CokioCode <cokiodecode@gmail.com>
2025-07-31 08:58:12 +0700

    fix(auth): change local to cookie

--- src/providers/TrpcProvider.tsx
+++ src/providers/TrpcProvider.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { useState } from "react";
 import { trpc } from "../lib/trpc";
+import Cookies from "js-cookie";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
@@ -14,7 +15,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
           url: "/api/trpc",
           headers() {
             if (typeof window !== "undefined") {
-              const token = localStorage.getItem("token");
+              const token = Cookies.get("token");
               return token ? { authorization: `Bearer ${token}` } : {};
             }
             return {};
